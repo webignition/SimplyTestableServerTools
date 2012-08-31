e57f698a699882ae9eb0275654fe63413d7c1a57
Jon Cram <jon@webignition.net>
2012-08-31 15:47:42 +0100

    Allow specific worker set to be started

--- src/SimplyTestable/ServerTools/Command/Command.php
+++ src/SimplyTestable/ServerTools/Command/Command.php
@@ -3,6 +3,60 @@
 namespace SimplyTestable\ServerTools\Command;
 
 use Symfony\Component\Console\Command\Command as BaseCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
-abstract class Command extends BaseCommand {    
+abstract class Command extends BaseCommand {
+    
+    /**
+     *
+     * @var InputInterface 
+     */
+    private $input;
+    
+    /**
+     *
+     * @var OutputInterface 
+     */
+    private $output;
+    
+    
+    /**
+     *
+     * @param InputInterface $input 
+     */
+    protected function setInput(InputInterface $input)
+    {
+        $this->input = $input;
+    }
+    
+    
+    /**
+     *
+     * @param OutputInterface $output 
+     */
+    protected function setOutput(OutputInterface $output)
+    {
+        $this->output = $output;
+    }
+
+
+    /**
+     *
+     * @return InputInterface 
+     */
+    protected function getInput()
+    {
+        return $this->input;
+    }
+    
+    
+    /**
+     *
+     * @return OutputInterface 
+     */
+    protected function getOutput()
+    {
+        return $this->output;
+    }
 }
--- src/SimplyTestable/ServerTools/Command/ResqueWorkersStartCommand.php
+++ src/SimplyTestable/ServerTools/Command/ResqueWorkersStartCommand.php
@@ -47,16 +47,54 @@ EOF
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $workerSet = $input->getOption('workerset');
-        if (!is_null($workerSet) && !in_array($workerSet, $this->getWorkerSetNames())) {
-            $output->writeln('workset "'.$workerSet.'" not valid, check app/config.json for valid options');
+        $this->setInput($input);
+        $this->setOutput($output);
+        
+        if (!$this->isWorkerSetOptionValid()) {
+            $output->writeln('workset "'.$this->getWorkersetOption().'" not valid, check app/config.json for valid options');
             return false;
         }
         
         $output->setDecorated(true);
-        foreach ($this->getWorkerSets() as $name => $workerSetDetails) {
-            $command = 'cd ' . $workerSetDetails->path . ' && ' . $this->getApplication()->getConfiguration()->{'resque-workers'}->commands->start->{$workerSetDetails->type};
-            exec($command . ' 2>&1 &');
+        foreach ($this->getWorkerSets() as $name => $workerSetDetails) {            
+            if (!$this->hasWorkerSetOption() || ($this->hasWorkerSetOption() && $name == $this->getWorkersetOption())) {
+                $this->getOutput()->writeln('Starting workers for: ' . $name);
+
+                $command = 'cd ' . $workerSetDetails->path . ' && ' . $this->getApplication()->getConfiguration()->{'resque-workers'}->commands->start->{$workerSetDetails->type};
+                $this->getOutput()->writeln('Running command: ' . $command);
+                exec($command . ' 2>&1 &');     
+            }
         }
+    }    
+    
+    /**
+     *
+     * @return string|null
+     */
+    private function getWorkersetOption()
+    {
+        return $this->getInput()->getOption('workerset');
+    }
+    
+    
+    /**
+     *
+     * @return boolean 
+     */
+    private function isWorkerSetOptionValid() {
+        if (!$this->hasWorkerSetOption()) {
+            return true;
+        }
+        
+        return in_array($this->getWorkersetOption(), $this->getWorkerSetNames());
+    }
+    
+    
+    /**
+     *
+     * @return boolean 
+     */
+    private function hasWorkerSetOption() {
+        return !is_null($this->getWorkersetOption());
     }
 }
