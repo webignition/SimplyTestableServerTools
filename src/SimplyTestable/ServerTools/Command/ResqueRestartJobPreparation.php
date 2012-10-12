c8cf6c26be4222d208268264a5b97c746199c6f4
Jon Cram <jon@webignition.net>
2012-10-12 19:36:36 +0100

    Add command to restart job prep queue worker if stuck

---
+++ src/SimplyTestable/ServerTools/Command/ResqueRestartJobPreparation.php
@@ -0,0 +1,39 @@
+<?php
+namespace SimplyTestable\ServerTools\Command;
+
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ResqueWorkersStopCommand extends AbstractResqueWorkersCommand
+{
+    
+    protected function configure()
+    {
+        $this
+            ->setName('resque:restart-job-preparation')
+            ->setDescription('Restart job preparation resque worker if stuck')
+            ->setHelp(<<<EOF
+Restart job preparation resque worker if stuck
+EOF
+        );
+    }
+    
+    
+    protected function executeForWorkerset($name, $workerSetDetails) {
+        $jobPreparationQueueLogName = '/home/simplytestable/www/app.simplytestable.com/app/logs/resque-jobs.log';
+        
+        $jobPreparationQueueLogSizeBefore = filesize($jobPreparationQueueLogName);
+        
+        sleep(10);
+        
+        $jobPreparationQueueLogSizeAfter = filesize($jobPreparationQueueLogName);
+        
+        if ($jobPreparationQueueLogSizeAfter - $jobPreparationQueueLogSizeBefore == 0) {
+            passthru('cd /home/simplytestable/www/tools && php app/console resque:workers:stop --workerset app-job-prepare');
+            sleep(5);
+            passthru('cd /home/simplytestable/www/tools && php app/console resque:workers:start --workerset app-job-prepare');
+        }
+    }
+}
